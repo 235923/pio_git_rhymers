6640cf93a5800c501b3d658753a67851dc1177c2
Wiktor Michałowski <235923@edu.p.lodz.pl>
2022-05-09 13:36:31 +0200

    16. Dokumentacja

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,6 +1,10 @@
 package edu.kis.vh.nursery;
 
+/**
+ * rhymes
+ */
 public class DefaultCountingOutRhymer {
+
 
 	private static final int INT = 12;
 	private static final int INT1 = -1;
@@ -11,25 +15,38 @@ public class DefaultCountingOutRhymer {
 
 	private int total = INT1;
 
+
+	/**
+	 * adds number
+	 */
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
+	/**
+	 * checks if empty
+	 */
 	public boolean callCheck() {
 			return total == INT1;
 		}
-		
+
+	/**
+	 * checks if full*/
 	public boolean isFull() {
 				return total == INT2;
 			}
-		
+
+	/**
+	 * returns last number*/
 	protected int peekaboo() {
 		if (callCheck())
 			return INT1;
 		return numbers[total];
 	}
 
+	/**
+	 * returns and then deletes the last number*/
 	public int countOut() {
 		if (callCheck())
 			return INT1;
